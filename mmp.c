34c593edd6340a18daae1371e8d6b803fbf41789
nongzhuan555 <142788697+nongzhuan555@users.noreply.github.com>
2023-10-09 21:03:52 +0800

    mmp
    have no words to say

---
+++ mmp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+    void main()
+    {
+        int x,y;
+        printf("请输入初始值x：");
+        scanf("%d",&x);
+        if(x>=10)
+        
+            y=x;
+            printf("x=%d",x);
+        else
+        
+                    y=2*x+1;
+                    printf("x=%d",x);
+        
+        
+    }
